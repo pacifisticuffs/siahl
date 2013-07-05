3a4be70788e4fcda9fc64aa7dbe891f5964536ee
pacifisticuffs <tim.lynn@gmail.com>
2013-07-05 08:33:30 -0700

    More view work, added rink rats

--- siahl_app/views.py
+++ siahl_app/views.py
@@ -2,6 +2,7 @@ from django.template import Template, Context
 from django.http import HttpResponse
 from django.shortcuts import render
 from siahl_app import models
+from django.db.models import Count, Sum, Avg, Max
 
 def index(request):
   players = models.PlayerStat.objects.raw('''
@@ -12,4 +13,28 @@ def index(request):
     limit 10
   ''')
 
-  return render(request, 'index.html', { 'players' : players })
+  rats = models.PlayerStat.objects.raw('''
+    SELECT *, COUNT(`siahl_app_playerstat`.`player_id`) AS `count`
+    FROM `siahl_app_playerstat`
+    INNER JOIN siahl_app_player ON (siahl_app_playerstat.player_id = siahl_app_player.id)
+    WHERE siahl_app_player.goalie=False
+    GROUP BY `siahl_app_playerstat`.`player_id`
+    HAVING COUNT(`siahl_app_playerstat`.`player_id`) > 4
+    ORDER BY count desc
+  ''')
+
+  return render(request, 'index.html', { 'players' : players, 'rats' : rats })
+
+def rinkrats(request):
+  players = models.PlayerStat.objects.raw('''
+    SELECT *, COUNT(`siahl_app_playerstat`.`player_id`) AS `count`
+    FROM `siahl_app_playerstat`
+    INNER JOIN siahl_app_player ON (siahl_app_playerstat.player_id = siahl_app_player.id)
+    WHERE siahl_app_player.goalie=False
+    GROUP BY `siahl_app_playerstat`.`player_id`
+    HAVING COUNT(`siahl_app_playerstat`.`player_id`) > 4
+    ORDER BY count desc
+  ''')
+
+  return render(request, 'rinkrats.html', { 'players' : players})
+
